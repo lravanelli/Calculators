dfeef60e1ddb600b709b5bf3a052d8511697f2eb
lravanelli <ravanelli.luis@gmail.com>
2017-12-14 15:42:45 -0200

    Função no Botão

--- app/src/main/java/com/example/logonrm/calculators/fragments/IMCfragment.java
+++ app/src/main/java/com/example/logonrm/calculators/fragments/IMCfragment.java
@@ -6,7 +6,9 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import com.example.logonrm.calculators.R;
 
@@ -22,6 +24,7 @@ public class IMCfragment extends Fragment {
 
 
     private EditText etAltura;
+    private TextView tvIMC;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -30,7 +33,19 @@ public class IMCfragment extends Fragment {
 
         View v = inflater.inflate(R.layout.fragment_imcfragment, container, false);
         etAltura = (EditText)v.findViewById(R.id.etAltura);
+        tvIMC = (TextView)v.findViewById(R.id.tvIMC);
+
+        Button bt = (Button)v.findViewById(R.id.btCalcular);
+        bt.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                tvIMC.setText("Seu IMC é: Fudido");
+            }
+        });
+
         return v;
     }
+
+
 
 }
